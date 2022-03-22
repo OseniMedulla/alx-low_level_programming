1453bf2675429a20c9281f7df09b83922d9f127d
OseniMedulla <osenimedulla01@gmail.com>
2022-03-22 15:15:19 -0700

    Printing strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts - function to print string
+ *
+ * @str : string to be printed
+ *
+ * Return : void
+ */
+void _puts(char *str)
+{
+	int = 1;
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
