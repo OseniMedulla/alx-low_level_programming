4f4cd9c786377926075618a8707e9dc5481aaf00
OseniMedulla <osenimedulla01@gmail.com>
2022-03-29 16:29:54 -0700

    Printing a chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,18 +8,16 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int y, x;
+	int x;
+	int y;
 
-	y = 0;
-	while (y < 8)
+	for (x = 0; x < 8; x++)
 	{
-		x = 0;
-		while (x < 8)
+		for (y = 0; y < 8; y++)
 		{
-			_putchar(a[y][x]);
-			x++;
+			_putchar(a[x][y]);
+			if (y == 7)
+				_putchar('\n');
 		}
-		_putchar('\n');
-		y++;
 	}
 }
