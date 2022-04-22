e1192c38c5b4ec65d15787c3115bbdf6103ea401
OseniMedulla <osenimedulla01@gmail.com>
2022-04-22 15:04:46 -0700

    alright

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns length of a list
+ * @h: the list
+ * Return: length of the list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
