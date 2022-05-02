1f91c895b4edf2b03f8a9f2542d5aa4f8ceda83a
OseniMedulla <osenimedulla01@gmail.com>
2022-05-02 15:49:53 -0700

    commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,12 @@
 #ifndef MAIN_H_
 #define MAIN_H_
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include <stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+
 
 int _putchar(char);
 ssize_t read_textfile(const char *filename, size_t letters);
