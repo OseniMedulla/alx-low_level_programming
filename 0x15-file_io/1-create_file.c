0d254b5693c9d83cd61b5063eac0cfbe5110f987
OseniMedulla <osenimedulla01@gmail.com>
2022-05-03 09:48:44 -0700

    well commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,28 @@
 #include "main.h"
 
+/**
+ * _strlen - finds the length of a string
+ * @str: pointer to the string
+ * Return: length of the string
+ */
+
+size_t _strlen(char *str)
+{
+	size_t i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * create_file - creates a file.
+ * @filename: name of the file to be created
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure (file can not be created,
+ * file can not be written, write “fails”
+ */
+
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
